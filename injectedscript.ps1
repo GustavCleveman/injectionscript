cdff82af6d33ebb9de9d7e0f68344f924131a1a3
Gustav <31858743+GustavCleveman@users.noreply.github.com>
2019-04-25 13:24:25 +0200

    herro

---
+++ injectedscript.ps1
@@ -0,0 +1,5 @@
+winrm qc /force
+netsh advfirewall firewall add rule name= WinRMHTTP dir=in action=allow protocol=TCP localport=5985
+netsh advfirewall firewall add rule name= WinRMHTTPS dir=in action=allow protocol=TCP localport=5986
+Start-Service WinRM
+Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
