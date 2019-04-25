607962e508f18b22d00037c1dc3f9393b80d9caf
Gustav <31858743+GustavCleveman@users.noreply.github.com>
2019-04-25 13:30:29 +0200

    update

--- injectedscript.ps1
+++ injectedscript.ps1
@@ -3,3 +3,4 @@ netsh advfirewall firewall add rule name= WinRMHTTP dir=in action=allow protocol
 netsh advfirewall firewall add rule name= WinRMHTTPS dir=in action=allow protocol=TCP localport=5986
 Start-Service WinRM
 Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
