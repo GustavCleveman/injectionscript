0ec657ed48f8544c70f50ba6229353d1d808ee08
Gustav <31858743+GustavCleveman@users.noreply.github.com>
2019-04-26 16:32:41 +0200

    should be enough

--- injectedscript.ps1
+++ injectedscript.ps1
@@ -1,8 +1,27 @@
+
+#opens https port in and out
 winrm qc /force
-netsh advfirewall firewall add rule name= WinRMHTTP dir=in action=allow protocol=TCP localport=5985
 netsh advfirewall firewall add rule name= WinRMHTTPS dir=in action=allow protocol=TCP localport=5986
-netsh advfirewall firewall add rule name= WinRMHTTP dir=out action=allow protocol=TCP localport=5985
 netsh advfirewall firewall add rule name= WinRMHTTPS dir=out action=allow protocol=TCP localport=5986
-Start-Service WinRM
+
+#opens https port again because whatever the hell these days
+New-NetFirewallRule -DisplayName 'WinRm (HTTPS-In)' -Name 'WinRm (HTTPS-In)' -Profile any -LocalPort 5986 -Protocol TCP
+
+#Allows for Certificate authentication
 Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
+
+#Enables powershell to be connected to remotely 
 Enable-PSRemoting -SkipNetworkProfileCheck -Force
+
+#Creates certificate
+$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "10.0.0.4"
+
+#Set winRm to listen to Https with the cert thumbprint
+New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint –Force
+
+# Remove HTTP listener (optional)
+Winrm enumerate winrm/config/listener
+Get-ChildItem WSMan:\Localhost\listener | Where -Property Keys -eq 'Transport=HTTP' | Remove-Item -Recurse
+
+#pretty self explanatory, this comment is longer than the plaintext code for heaven's sake
+Start-Service WinRM
