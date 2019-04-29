fc4fe9a8f264d2ec9d365749f515a01977d05514
Gustav <31858743+GustavCleveman@users.noreply.github.com>
2019-04-29 17:12:39 +0200

    hej

--- injectedscript.ps1
+++ injectedscript.ps1
@@ -1,8 +1,9 @@
-
 #opens https port in and out
 winrm qc /force
 netsh advfirewall firewall add rule name= WinRMHTTPS dir=in action=allow protocol=TCP localport=5986
 netsh advfirewall firewall add rule name= WinRMHTTPS dir=out action=allow protocol=TCP localport=5986
+netsh advfirewall firewall add rule name= WinRMHTTPS dir=in action=allow protocol=TCP localport=5985
+netsh advfirewall firewall add rule name= WinRMHTTPS dir=out action=allow protocol=TCP localport=5985
 
 #opens https port again because whatever the hell these days
 New-NetFirewallRule -DisplayName 'WinRm (HTTPS-In)' -Name 'WinRm (HTTPS-In)' -Profile any -LocalPort 5986 -Protocol TCP
@@ -12,22 +13,20 @@ New-NetFirewallRule -DisplayName 'WinRm (HTTPS-In)' -Name 'WinRm (HTTPS-In)' -Pr
 
 #Enables powershell to be connected to remotely 
 Enable-PSRemoting -SkipNetworkProfileCheck -Force
-Start-Service WinRM
 
 #Creates certificate
-$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "10.0.0.4"
+$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "10.0.0.4" -Type DocumentEncryptionCert, SSLServerAuthentication -Force
 #$CertPath = "Cert:\LocalMachine\My\$Cert.Thumbprint"
 
 $Pswrd = ConvertTo-SecureString -String "1Treetop2!" -Force -AsPlainText
 
 Export-PfxCertificate -Cert $Cert -FilePath c:\selfCert.pfx -Password $Pswrd
 
-
-
 #Set winRm to listen to Https with the cert thumbprint
 New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
+Enable-WSManCredSSP -Role server
 
-# Remove HTTP listener (optional)
+#Remove HTTP listener (optional)
 #Winrm enumerate winrm/config/listener
 #Get-ChildItem WSMan:\Localhost\listener | Where -Property Keys -eq 'Transport=HTTP' | Remove-Item -Recurse
 
